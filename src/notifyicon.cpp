d693bd9c6f9fbf945a527146ee142894f327780f
jesperhh <jesperhh@gmail.com>
2014-06-20 21:45:54 +0200

    Use old style swprintf with mingw

--- src/notifyicon.cpp
+++ src/notifyicon.cpp
@@ -27,8 +27,13 @@ NotifyIcon::NotifyIcon(HINSTANCE hInstance, HWND hWnd)
     notifyIconData.uVersion = NOTIFYICON_VERSION;
     notifyIconData.dwInfoFlags = NIIF_INFO;
 
+    #ifdef __MINGW32__
+    ::swprintf(notifyIconData.szTip, _T("WinK760"));
+    ::swprintf(notifyIconData.szInfoTitle, _T("WinK760"));
+    #else
     ::swprintf(notifyIconData.szTip, 128, _T("WinK760"));
     ::swprintf(notifyIconData.szInfoTitle, 64, _T("WinK760"));
+    #endif // !LIM_SMALL
 
     Shell_NotifyIcon(NIM_ADD, &notifyIconData);
 }
@@ -42,6 +47,10 @@ NotifyIcon::~NotifyIcon(void)
 void NotifyIcon::Balloon(const TCHAR* info)
 {
     notifyIconData.uFlags |= NIF_INFO;
+#ifdef __MINGW32__
+    ::swprintf(notifyIconData.szInfo, info);
+#else
     ::swprintf(notifyIconData.szInfo, 256, info);
+#endif // !LIM_SMALL
     Shell_NotifyIcon(NIM_MODIFY, &notifyIconData);
 }
